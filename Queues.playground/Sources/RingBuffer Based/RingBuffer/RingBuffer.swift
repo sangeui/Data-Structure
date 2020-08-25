0876c298d466d6b0372acc699cf9f48ce3a589d6
Sangeui <Sangeuis@icloud.com>
2020-08-25 16:34:14 +0900

    Added RB based Queue

---
+++ Queues.playground/Sources/RingBuffer Based/RingBuffer/RingBuffer.swift
@@ -0,0 +1,59 @@
+import Foundation
+
+public struct RingBuffer<T> {
+    private var array: [T?]
+    private var readIndex = 0
+    private var writeIndex = 0
+    
+    public init(count: Int) {
+        array = [T?](repeating: nil, count: count)
+    }
+}
+extension RingBuffer {
+    // Ring buffer 쓰기 연산
+    public mutating func write(_ element: T) -> Bool {
+        if !isFull {
+            array[writeIndex % array.count] = element; writeIndex += 1
+            return true
+        } else {
+            return false
+        }
+    }
+}
+extension RingBuffer {
+    // Ring buffer 읽기 연산
+    public mutating func read() -> T? {
+        if !isEmpty {
+            let element = array[readIndex & array.count]; readIndex += 1
+            return element
+        } else {
+            return nil
+        }
+    }
+}
+extension RingBuffer {
+    private var availableSpaceForReading: Int {
+        return writeIndex - readIndex
+    }
+    private var availableSpaceForWriting: Int {
+        return array.count - availableSpaceForReading
+    }
+    public var isEmpty: Bool {
+        return availableSpaceForReading == 0
+    }
+    public var isFull: Bool {
+        return availableSpaceForWriting == 0
+    }
+    public var first: T? {
+        if !isEmpty {
+            return array[readIndex & array.count]
+        } else {
+            return nil
+        }
+    }
+}
+extension RingBuffer: CustomStringConvertible {
+    public var description: String {
+        String(describing: array)
+    }
+}
